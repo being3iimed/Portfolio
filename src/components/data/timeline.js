a245f2fc068f161faa78919b9bf9ab39af7dcb8a
being3iimed <imed.kouki@rades.r-iset.tn>
2023-03-31 17:26:56 +0200

    adding info into timeline.js

---
+++ src/components/data/timeline.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        Year : "2023", 
+        Title: "Software Developer Intern",
+        Duration : "Jan 2023 - Present",
+        Company :"ADACTIM | WEEVIO Group",
+        Description : "My role is to automate the supervision of our IT system through RPA technology. I work collaboratively with stakeholders to identify areas where automation can be applied and then design and develop RPA solutions from scratch to improve business efficiency and accuracy.",
+    },
+    {
+        Year : "2023", 
+        Title: "Solo Indie Developer",
+        Description : "I'm a solo indie developer I create apps for my personal use, starting from the initial stages of planning and design, and continuing all the way through to utilizing code to solve practical issues. Each coding project is developed from scratch.",
+    },
+    {
+        Year : "2022", 
+        Title: "Full-Stack Developer Intern",
+        Company :"SILICOD.",
+        Duration : "Aug 2022 - Sep 2022",
+        Description : "I created a chat app to enhance my skills and gain practical experience in software development, learning about different programming languages and technologies.",
+    },
+    {
+        Year : "2021", 
+        Title: "IT Intern",
+        Company :"DRÄXLMAIER Group",
+        Duration : "Sep 2021 - Oct 2021",
+        Description : " I assisted with hardware and software installations, upgrades, and troubleshooting. This hands-on experience improved my technical skills and problem-solving abilities.",
+    },
+    {
+        Year : "2020 - 2023 ", 
+        Title: "Bachelor's Degree in Information Technology",
+        Duration : "3 years",
+        Description : "My pursuit of an Information Technology degree has provided me with knowledge in various fields, including computer architecture, computer science theory, a range of programming languages, and aspects of artificial intelligence.",
+    },
+]
