651b719a749627905d9edcc30865d64356ed2f75
being3iimed <imed.kouki@rades.r-iset.tn>
2023-04-01 15:36:51 +0200

    changes on Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-function Footer(){
-    return (
-        <div></div>
-    )
-}
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="bg-white py-16 text-stone-900">
+      <div className="text-center">
+        <p className="text-primary font-normal text-base mb-2 dark:text-white">
+          © {currentYear} Imed Kouki. All rights reserved.
+        </p>
+        <p className="text-primary font-semibold text-base dark:text-white">
+          Made with {"<3"}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
