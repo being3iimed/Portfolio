f1f8a31b4ef8802b1414070ef70667aaf5783b30
being3iimed <imed.kouki@rades.r-iset.tn>
2023-04-03 01:16:28 +0200

    DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Intro from './components/intro';
+import Intro from './components/Intro';
 import Portfolio from './components/Portfolio';
 import Timeline from './components/Timeline';
 
